2aa817d9363534da19ea8a9373c4b23e155b9642
Nkosikenny <kennybhekumuzi@gmail.com>
2025-05-23 19:26:27 +0200

    initialized repository and created product card component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import ProductCard from './components/ProductCard'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <ProductCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <>
+      <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <h4 className='text-2xl'>title</h4>
+            <p className='text-base'>price </p>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default ProductCard
