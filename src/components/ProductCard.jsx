2e231c6ff096747cfc76b57432bf684763b4297f
Nkosikenny <kennybhekumuzi@gmail.com>
2025-08-06 08:43:42 +0200

    Remove cart context and clean up unused imports

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 
 const ProductCard = ({ title, price, description, image }) => {
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import { cartContext } from "../context/Cart";
+
 
 const ProductList = () => {
   const [productData, setProductData] = useState([]);
